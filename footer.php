841f1fa5d7edae3d5026d37fd4a9d3d061699d4e
Robin BIDOT <rbidot@hotmail.fr>
2024-01-18 10:48:48 +0100

    Icônes sociaux

--- footer.php
+++ footer.php
@@ -16,8 +16,8 @@ echo '<footer class="container-fluid bg-dark text-white mt-5">
             <div class="col-md-4">
                 <h5>Contactez-nous</h5>
                 <ul class="list-unstyled">
-                    <li><i class="fas fa-envelope"></i> Email: info@example.com</li>
-                    <li><i class="fas fa-phone"></i> Téléphone: +123456789</li>
+                    <li><i class="fas fa-envelope"></i> Email: contact.siliconesavvy@example.com</li>
+                    <li><i class="fas fa-phone"></i> Téléphone: +33 1 99 99 99 99</li>
                 </ul>
             </div>
         </div>
@@ -50,24 +50,27 @@ echo '<footer class="container-fluid bg-dark text-white mt-5">
             <div class="col-md-4">
                 <h5>Suivez-nous sur</h5>
                 <ul class="list-unstyled">
-                    <li><a href="#"><i class="fab fa-facebook-f"></i> Facebook</a></li>
-                    <li><a href="#"><i class="fab fa-twitter"></i> Twitter</a></li>
-                    <li><a href="#"><i class="fab fa-instagram"></i> Instagram</a></li>
-                    <li><a href="#"><i class="fab fa-linkedin-in"></i> LinkedIn</a></li>
-                    <li><a href="#"><i class="fab fa-tiktok"></i> TikTok</a></li>
-                    <li><a href="#"><i class="fab fa-youtube"></i> YouTube</a></li>
+                    <li><a href="https://www.facebook.com/"><img class="icones_sociaux" src="./assets/icones/facebook.svg" alt="Facebook"></a></li>
+                    <li><a href="https://twitter.com/X"><img class="icones_sociaux" src="./assets/icones/twitter.svg" alt="Twitter"></a></li>
+                    <li><a href="https://www.instagram.com/"><img class="icones_sociaux" src="./assets/icones/instagram.svg" alt="Instagram"></a></li>
+                    <li><a href="https://www.linkedin.com/"><img class="icones_sociaux" src="./assets/icones/linkedin.svg" alt="LinkedIn"></a></li>
+                    <li><a href="https://www.tiktok.com/fr"><img class="icones_sociaux" src="./assets/icones/tiktok.svg" alt="TikTok"></a></li>
+                    <li><a href="https://www.youtube.com/"><img class="icones_sociaux" src="./assets/icones/youtube.svg" alt="YouTube"></a></li>
                 </ul>
             </div>
             <div class="col-md-4">
                 <h5>Télécharger notre application</h5>
                 <ul class="list-unstyled">
-                    <li><a href="#"><img src="app-store-logo.png" alt="App Store"></a></li>
-                    <li><a href="#"><img src="google-play-logo.png" alt="Google Play"></a></li>
+                    <li><a href="https://play.google.com/store/apps?hl=fr&gl=US&pli=1"><img class="appstore" src="./assets/icones/appstore.png" alt="App Store"></a></li>
+                    <li><a href="https://www.apple.com/fr/app-store/"><img class="googleplay" src="./assets/icones/Google_Play_Store_badge_EN.svg.png" alt="Google Play"></a></li>
+                    <li><a href="https://www.google.com/"><img class="codeqr" src="./assets/icones/QR_Code_example.png" alt="QR code de l\'application"></a></li>
                 </ul>
             </div>
             <div class="col-md-4">
-                <p>&copy; 2024 Entreprise</p>
-                <p>Notre note sur Trustpilot : <a href="#"><img src="trustpilot-logo.png" alt="Trustpilot"></a></p>
+                <p>&copy; Silicone Savvy, tous droits réservés <br>
+                Dernière mise à jour du site internet le : <br>
+                24/01/2024</p>
+                <p>Notre note sur Trustpilot : <a href="https://fr.trustpilot.com/"><img class="trustpilot" src="./assets/icones/note_trustpilot.png" alt="Trustpilot"></a></p>
             </div>
         </div>
     </div>
